10a913cd2e28d0c754b85318063a842a74e0fb63
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-07-19 21:28:19 +0700

    update: metadata.title

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+
 import "./globals.css";
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "<gal/>: Portfolio",
+  title: "Gal's Portfolio",
   description: "This is Galih's portfolio. ",
 };
 
