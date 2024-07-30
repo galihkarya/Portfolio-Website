5c672bf0ce4fcab9f101e6d57d16036996806f09
Galih Karya Gemilang <galihkaryagemilang2001@gmail.com>
2024-07-30 21:09:15 +0700

    Update layout.tsx

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/"/>
+      </head>
       <body className={jakarta.className}>{children}</body>
     </html>
   );
